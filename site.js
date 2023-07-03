c112f29059d314bda89fd45dd50b0b4ce35ef74c
Juliana Lira <130653937+julianalirabr@users.noreply.github.com>
2023-07-03 22:58:39 +1000

    Add site files
    Final project website

---
+++ site.js
@@ -0,0 +1,195 @@
+// I used JSON to store and to populate the information in my website.
+
+let cityMelbourne = {
+  name: "Melbourne",
+  country: "Australia",
+  flightsAndNights: "Flights + 3 nights",
+  price: "$899",
+  imageUrl:
+    "https://lh3.googleusercontent.com/pw/AJFCJaUowx0Vw2GNICciNSk9u4jgeWGSjpqenRdgevwO9P9z4-rUm_B0beZjlbYLuf4zOG3rSiDardifNBm54YzXVeNxoh3Hrawj7Ig854kVZsfubpHGSAriarIYFCoXhnuiauu3IUb-o8Tls0qNSAdIgWeyWQ=w1500-h1000-s-no",
+  description:
+    "Melbourne, Australia is a vibrant and diverse city that offers something for everyone. Located in the southeastern part of the country, Melbourne is known for its stunning architecture, world-class restaurants, and thriving arts and culture scene.<br><br>Visitors to Melbourne can explore the city's many neighborhoods, each with its own unique charm and character. From the bustling streets of the CBD (Central Business District) to the trendy cafes and boutiques of Fitzroy and Brunswick, there is no shortage of things to see and do.<br><br>Melbourne is also home to numerous iconic landmarks, such as the Royal Botanic Gardens, Federation Square, and the Shrine of Remembrance. For sports fans, the city is famous for its love of Australian Rules Football (AFL) and is home to the Melbourne Cricket Ground (MCG), which is one of the largest stadiums in the world."
+};
+let citySydney = {
+  name: "Sydney",
+  country: "Australia",
+  flightsAndNights: "Flights + 5 nights",
+  price: "$1,133",
+  imageUrl:
+    "https://lh3.googleusercontent.com/pw/AJFCJaUz1orYoEpT6oarPvIOYNhS4_aj_3kqyjDa2_KiutwQF_wGztESOSPkk8ogthnRY9qdn_bwVwS6_TYo4z-Laz4CVyO-lD3_zJBQfeABUF8NgAYG7NlgQFNAZW0HIOm6oJb5xWdlJX3QBcNu3Tgjf3R_Bw=w1600-h1066-s-no",
+  description:
+    "Sydney, the capital of New South Wales, is a vibrant and cosmopolitan city that offers an unforgettable experience for visitors. Located on the east coast of Australia, Sydney is famous for its stunning beaches, iconic landmarks, and world-class dining and shopping.<br><br>One of the must-visit attractions in Sydney is the Sydney Opera House, a masterpiece of modern architecture that has become a symbol of the city. Visitors can take a guided tour of the building or attend a performance at one of the many theaters inside. Nearby, the Sydney Harbour Bridge offers breathtaking views of the city skyline and the harbor.<br><br>Sydney's beaches are among the most beautiful in the world, with Bondi Beach being the most famous. It's a great place to surf, swim, or simply soak up the sun. Other popular beaches include Manly, Coogee, and Bronte, each with its own unique character and charm."
+};
+let cityMaldives = {
+  name: "Sun Siyam Iru Veli",
+  country: "Maldives",
+  flightsAndNights: "Flights + 3 nights",
+  price: "$5,899",
+  imageUrl:
+    "https://lh3.googleusercontent.com/pw/AJFCJaUrSd8Q4lFEsDuYMLBvDj3DiVQ2f49rN-AgNkeUlSUgdchIdIOhbupGDhf31yhxVASqXpAsT4BMvKMnmrNMRF4xde6fNBRcGKoCYuFFKJQmec9QnUqBEti_8qotIGaN5ouhULe0biRGD_9YAAECWaEQ6Q=w1400-h933-s-no",
+  description:
+    "The Maldives is a tropical paradise located in the Indian Ocean, consisting of 26 coral atolls and over 1,000 islands. With its crystal-clear turquoise waters, pristine white sand beaches, and year-round warm weather, the Maldives is a dream destination for travelers seeking a luxurious and relaxing vacation.<br><br>Aside from its stunning natural beauty, the Maldives is also home to an incredible underwater world, with colorful coral reefs teeming with diverse marine life, making it a top destination for snorkeling and diving enthusiasts. The Maldives also offers a range of other activities, including water sports, island hopping, spa treatments, and romantic sunset cruises.<br><br>The Maldives is renowned for its world-class resorts, many of which are situated on private islands, offering exclusive experiences and unparalleled luxury. From overwater bungalows to beachfront villas, the Maldives has a variety of accommodation options to suit every traveler's needs."
+};
+let citySwitzerland = {
+  name: "Zermatt",
+  country: "Switzerland",
+  flightsAndNights: "Flights + 7 nights",
+  price: "$4,579",
+  imageUrl:
+    "https://lh3.googleusercontent.com/pw/AJFCJaVA0yakUcVVn0a1Lljcp0Ea8F0D43_3_4R2wrrUHX68gPXLJJWbcTcgqRGWA8ByFv4ByERWvForb_9QRhTbr6dnpUTTKRwuY2oDW_4wPTDZTVtNokxtGeeuPZiWZmSLylcMP4sxXoizx-5YojysqoPvFA=w840-h556-s-no",
+  description:
+    "Zermatt is a picturesque village located at the foot of the majestic Matterhorn mountain in Switzerland. With its charming narrow streets, cozy cafes and restaurants, and traditional Swiss chalets, Zermatt offers a perfect blend of alpine charm and modern amenities.<br><br>The village is a paradise for outdoor enthusiasts, offering a wide range of activities year-round, from skiing and snowboarding in the winter to hiking, mountain biking, and paragliding in the summer. The Matterhorn Glacier Paradise, Europe's highest cable car station, offers breathtaking panoramic views of the surrounding mountains and glaciers.<br><br>Zermatt is also renowned for its culinary scene, with numerous Michelin-starred restaurants and local specialties like Swiss cheese fondue and rösti. And for those seeking relaxation, the village boasts several luxurious spas and wellness centers."
+};
+let cityNewYork = {
+  name: "New York City",
+  country: "United States",
+  flightsAndNights: "Flights + 5 nights",
+  price: "$4,899",
+  imageUrl:
+    "https://lh3.googleusercontent.com/pw/AMWts8DaOzva1wqFM9_jmgZwOF0KVAIOYbroQoeVE8mdtLUW7efInWjghMH9wYda9otIS1eU0G00WPEz-WqSYXeSNPgFpQjbv8v_K6KT84mgIqPoRkjzyGDLTo_JiMFOv5K352SKVMquIwLNmMu6lEeWMExhww=w2061-h1374-no?authuser=0",
+  description:
+    "Welcome to the city that never sleeps! New York City is a vibrant metropolis located on the east coast of the United States. Known as the Big Apple, New York is home to over 8 million people and is one of the most iconic cities in the world.<br><br>New York is famous for its stunning skyline, world-class museums, diverse neighborhoods, and delicious food. You can explore famous landmarks like the Empire State Building, the Statue of Liberty, and Central Park.<br><br>The city is also a cultural hub, with a thriving arts scene, Broadway shows, and music venues. If shopping is your thing, New York has some of the best stores in the world, from luxury boutiques to vintage shops."
+};
+let citySingapore = {
+  name: "Marina Bay",
+  country: "Singapore",
+  flightsAndNights: "Flights + 4 nights",
+  price: "$2,679",
+  imageUrl:
+    "https://lh3.googleusercontent.com/pw/AJFCJaW_NdKp4zcfF_-X4AVgcwgdtHinl3-5IRYOSmaq2TFUD7B0GG1ohkn5JSUmPbF4H7azl912lvVtLJ5vkAjGmnIaAn9AzzikG5mlgDNrAaWAqCN-xWsLvp00dZjzkQaSu-0vGnrcNhYy0Ld7LNwTZ4T0yA=w1080-h675-s-no",
+  description:
+    "Marina Bay is one of the most iconic areas of Singapore, known for its stunning architecture, impressive skyline and a plethora of attractions. The area boasts some of the country's most famous landmarks, including the Marina Bay Sands hotel, the Merlion statue, and the futuristic Gardens by the Bay.<br><br>Visitors to Marina Bay can enjoy a wide range of activities, from shopping at the luxurious malls to dining at some of Singapore's most celebrated restaurants. The bay also features an array of cultural and entertainment options, such as the stunning light and water show, Spectra, and the breathtaking views from the top of the Marina Bay Sands SkyPark Observation Deck.<br><br>The area is also home to some of the city's most significant events, including the Formula 1 Singapore Grand Prix, which takes place each year on the Marina Bay Street Circuit. With its incredible mix of entertainment, attractions, and natural beauty, Marina Bay is an essential stop for any visitor to Singapore."
+};
+let cityJapan = {
+  name: "Tokyo",
+  country: "Japan",
+  flightsAndNights: "Flights + 10 nights",
+  price: "$4,769",
+  imageUrl:
+    "https://lh3.googleusercontent.com/pw/AJFCJaV_9oFoSJOTw7zefTg1FUuApZp3QuZApRukDEjoO2YE5iDzQzPtRf0nlWbBLxtuD5H8Lh937AJO_eHydRcDQ7nUdbBQm4G1mKTpB_ASlwQfblHPeO54rvVPW71IbXTC8sUeFDNKN-UvJcUddpBZWEiKmw=w1900-h1268-s-no",
+  description:
+    "Tokyo is a vibrant and bustling city that offers a unique blend of modernity and tradition. As the capital of Japan, it is one of the most populous cities in the world and is a hub of technology, fashion, and culture.<br><br>In Tokyo, you can find everything from towering skyscrapers to historic temples, sprawling shopping districts to serene parks and gardens. The city is known for its excellent cuisine, ranging from street food to Michelin-starred restaurants, and is a paradise for food lovers.<br><br>One of the most popular tourist attractions in Tokyo is the Tokyo Skytree, a towering 634-meter tower that offers stunning views of the city from its observation decks. Other must-see sights include the Imperial Palace, the Meiji Shrine, and the famous Shibuya Crossing, which is one of the busiest pedestrian crossings in the world.<br><br>"
+};
+let cityHawaii = {
+  name: "Waikiki Beach",
+  country: "Hawaii",
+  flightsAndNights: "Flights + 6 nights",
+  price: "$3,504",
+  imageUrl:
+    "https://lh3.googleusercontent.com/pw/AJFCJaXYgOy2SrJZdU3A_VDzbMJzLw5_Km14EuGQA_L99o4hUiKkwTl8TTtlPAvZdOOkoYq6tM3U15BLHTuqVhOG3ILJ_mLy0-d56K3wFOoullvnhz5btiq2sy-HSUpdFGWj3jw9-98HlNETpEZ80ryYQtuNEg=w1600-h1066-s-no",
+  description:
+    "Waikiki Beach is a stunning stretch of golden sand and crystal-clear waters located on the south shore of Honolulu, Hawaii. It is one of the most famous beaches in the world, attracting millions of visitors every year with its picturesque views and endless opportunities for fun and relaxation.<br><br>The beach is known for its gentle waves, making it an ideal spot for swimming, boogie boarding, and surfing for beginners. The beach is also lined with some of the best hotels, restaurants, and shops in Hawaii, offering visitors an exciting and convenient experience.<br><br>At Waikiki Beach, you can take a relaxing stroll along the shore, go for a swim, sunbathe on the sand, or take part in various water activities such as stand-up paddleboarding and kayaking. Additionally, the beach is a great place to catch a beautiful sunset over the ocean."
+};
+let cityCanada = {
+  name: "Whistler",
+  country: "Canada",
+  flightsAndNights: "Flights + 5 nights",
+  price: "$5,799",
+  imageUrl:
+    "https://lh3.googleusercontent.com/pw/AJFCJaVaHyqz6OBlRsm-BvvckIQgqZRaB5feWNh3afHBvZOVzoR0IUIbKNF3lNY7m5zJn14BouQLEbWetLAGCFJ31n0XaJMAWMWT0-Uvc_5jsEE7EzmX82BuhUCuLuTI3tGKS6Oj3hW5MVHgfTCBF9-P1tY4uw=w1280-h720-s-no",
+  description:
+    "Whistler, Canada is a picturesque mountain town nestled in the Coast Mountains of British Columbia. Renowned for its world-class skiing and snowboarding, Whistler is also a popular summer destination for hiking, mountain biking, and outdoor adventure. With stunning alpine scenery, lively village atmosphere, and plenty of outdoor activities, Whistler is a must-visit destination for any adventure seeker.<br><br>In the winter, Whistler Blackcomb Ski Resort offers an unparalleled skiing and snowboarding experience with over 8,000 acres of skiable terrain and a peak elevation of 2,284 meters. The village is a hub of activity with a wide range of restaurants, bars, and shops. In the summer, visitors can enjoy hiking, mountain biking, golfing, zip-lining, and a host of other outdoor activities. There are also many events and festivals throughout the year, including the famous Whistler Ski and Snowboard Festival in April.<br><br>Whistler has a variety of accommodation options to suit all budgets, from cozy cabins to luxurious resorts. The village is also home to a thriving arts and culture scene, with numerous galleries and museums to explore. With its stunning natural beauty and endless outdoor activities, Whistler is a destination that will leave you with unforgettable memories."
+};
+
+// all the cities together in an array
+let cities = [
+  cityMelbourne,
+  citySydney,
+  cityMaldives,
+  citySwitzerland,
+  cityNewYork,
+  citySingapore,
+  cityJapan,
+  cityHawaii,
+  cityCanada
+];
+
+function showSelectedTravelDetails(index) {
+  // First, I populate the HTML elements in the second page with the data from the selected travel.
+  let selectedTravel = cities[index];
+
+  document.querySelector("#selected-travel-title").innerHTML =
+    selectedTravel.name;
+  document.querySelector("#selected-travel-image").src =
+    selectedTravel.imageUrl;
+  document.querySelector("#selected-travel-image").alt =
+    selectedTravel.name + " - " + selectedTravel.country;
+  document.querySelector("#selected-travel-full-title").innerHTML =
+    selectedTravel.name + ", " + selectedTravel.country;
+  document.querySelector("#selected-travel-flights-and-nights").innerHTML =
+    selectedTravel.flightsAndNights;
+  document.querySelector("#selected-travel-description").innerHTML =
+    selectedTravel.description;
+  document.querySelector("#selected-travel-price").innerHTML =
+    selectedTravel.price;
+
+  //I also populate the city name in the modal window (third page)
+  document.querySelector("#selected-travel-confirmation-city-name").innerHTML =
+    selectedTravel.name;
+
+  // Then, I ensure only the second page (selected travel details) is shown. I do this by adding the "hide" class to the first page and the modal window, and removing the "hide" class from the second page.
+  document.querySelector("#travel-selection").classList.add("hide");
+  document.querySelector("#selected-travel-details").classList.remove("hide");
+  document.querySelector("#selected-travel-confirmation").classList.add("hide");
+}
+
+function showTravelSelection() {
+  // Ensure only the first page (travel selection) is shown. I do this by removing the "hide" class from the first page and adding the "hide" class to the second page and the modal window.
+  document.querySelector("#travel-selection").classList.remove("hide");
+  document.querySelector("#selected-travel-details").classList.add("hide");
+  document.querySelector("#selected-travel-confirmation").classList.add("hide");
+}
+
+function showModalWindow() {
+  // Then, I ensure the modal window is shown on top of the second page. I do this by adding the "hide" class to the first page, and removing the "hide" class from the second page and from the modal window.
+  document.querySelector("#travel-selection").classList.add("hide");
+  document.querySelector("#selected-travel-details").classList.remove("hide");
+  document
+    .querySelector("#selected-travel-confirmation")
+    .classList.remove("hide");
+}
+
+// Add event listener to all the items in the travel selection page.
+
+// All the different travel items are a <div> with an id in the format "item0", "item1", "item2", etc..
+// In here I am looping through the cities array so that I can add an event listener to all the travel items, making sure they call the showSelectedTravelDetails function with the correct array index.
+for (let i = 0; i < cities.length; i++) {
+  document.querySelector("#item" + i).addEventListener("click", function () {
+    showSelectedTravelDetails(i);
+  });
+}
+
+// event listener to the buttons in the travel details page (second page)
+document
+  .querySelector("#selected-travel-go-back")
+  .addEventListener("click", showTravelSelection);
+document
+  .querySelector("#selected-travel-i-love-it")
+  .addEventListener("click", showModalWindow);
+
+// event listener to the button in the modal window
+document
+  .querySelector("#selected-travel-confirmation-start-again")
+  .addEventListener("click", showTravelSelection);
+
+// side menu hamburger
+
+function SideMenuShow() {
+  document.querySelector("#side-menu").classList.add("show-menu");
+}
+
+function SideMenuHide() {
+  document.querySelector("#side-menu").classList.remove("show-menu");
+}
+
+document.querySelector("#menu-trigger").addEventListener("click", SideMenuShow);
+
+document.querySelector("#menu-close").addEventListener("click", SideMenuHide);
+
+// RESOURCES
+
+// resource used to create the logo (https://www.freelogodesign.org/)
+
+// colour palettes generator (https://coolors.co/)
+
+// hamburger (svg img) colour using filter: https://stackoverflow.com/a/53336754
